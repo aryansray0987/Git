985189c72a1260b08e0c29e095e5f0e01f4c7265
Aryan <aryansray0987@gmail.com>
2026-03-05 21:29:00 +0530

    added some feat

--- app.js
+++ app.js
@@ -24,5 +24,7 @@ function mul() {
 // git add README.md
 // git commit -m "first commit"
 // git branch -M main
-// git remote add origin https://github.com/aryansray0987/Git-learning.git
-// git push -u origin main
+// git remote add origin https://github.com/aryansray0987/Git-learning.git //this should run once for one porject
+// git push -u origin main // this should also run once for the one project
+
+// after that..... git push ........for the same project
