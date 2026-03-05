19242816e08e33943e37f2261c930058cb521e3c
Aryan <aryansray0987@gmail.com>
2026-03-05 15:10:22 +0530

    added mul function

--- app.js
+++ app.js
@@ -7,3 +7,22 @@ function add(a, b) {
 function substract() {
     return 'this function return something ...'
 }
+function mul() {
+    return 'this function multiply the numbers '
+}
+
+
+
+
+
+
+
+//These are the git commands ......
+
+// echo "# Git-learning" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/aryansray0987/Git-learning.git
+// git push -u origin main
